56d5194a260c96ca562a5e836119e0b72b43dfe2
anto__dev <antonidwm@gmail.com>
2022-05-18 02:00:14 +0100

    ADD content and styles for landing page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,21 @@
 console.info('Hello world');
+
+
+/// modification de l'état actif de la nav 
+
+let allLinkNav = document.querySelectorAll('.nav__el');
+
+allLinkNav.forEach(el => {
+    el.addEventListener('click', ()=>{
+        deleteActifState (el);
+        el.classList.add('nav__el--actif');
+        
+    });
+})
+
+function deleteActifState (el){
+   
+    allLinkNav.forEach(el => {
+        el.classList.remove('nav__el--actif')
+    });
+}
